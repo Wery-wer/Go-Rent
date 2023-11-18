1c04607344c99e4854c37dc7e9e15c590e85fe0a
Wery <44234831+Wery-wer@users.noreply.github.com>
2023-11-18 11:35:50 +0700

    Change Files

---
+++ src/main/java/com/mycompany/go/rentt/GoRentt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.go.rentt;
+
+/**
+ *
+ * @author Holanta
+ */
+public class GoRentt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
